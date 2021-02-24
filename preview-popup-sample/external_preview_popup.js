9cad1c4c9a02dcb82233d5b8270173dbdaae93af
antongrinenko <anton.grinenko@gmail.com>
2021-02-24 14:01:32 +0200

    Added preview popup example

---
+++ preview-popup-sample/external_preview_popup.js
@@ -0,0 +1,75 @@
+window.ExternalPreviewPopup = (function() {
+    var externalPreviewPopup;
+
+    var close = function() {
+        externalPreviewPopup.style.visibility = 'hidden';
+    };
+
+    var initPreviewPopup = function() {
+        var div = document.createElement('div');
+        div.innerHTML = '\
+            <div id="externalPreviewPopup">\
+                <div class="modal-container">\
+                    <div class="modal-header-container">\
+                        <div>\
+                           <button type="button" class="close modal-close-button">\
+                                <span>×</span>\
+                            </button>\
+                            <h4 class="modal-title">External Preview Popup</h4>\
+                        </div>\
+                    </div>\
+                    <div id="content" style="padding: 15px;" class="preview-container-fluid">\
+                       <div class="preview-row">\
+                            <div class="preview-col-sm-8">\
+                                <div class="esdev-desktop-device">\
+                                    <div class="esdev-email-window-panel">\
+                                        <div class="esdev-email-subject" style="min-height: 20px"></div>\
+                                    </div>\
+                                    <div class="esdev-desktop-device-screen">\
+                                        <iframe id="iframeDesktop" frameborder="0" width="100%" height="642"></iframe>\
+                                    </div>\
+                                </div>\
+                            </div>\
+                            <div class="preview-col-sm-4 esdev-no-padding-left">\
+                                <div class="esdev-mobile-device center-block">\
+                                    <div class="esdev-mobile-device-screen">\
+                                        <img src="mobile-view-top-bar.png" alt="">\
+                                        <iframe id="iframeMobile" frameborder="0" width="100%" height="459"></iframe>\
+                                        <img class="esdev-mail-bottom-bar" src="mobile-view-bottom-bar.png" alt="">\
+                                    </div>\
+                                </div>\
+                            </div>\
+                       </div>\
+                    </div>\
+                </div>\
+            </div>';
+        document.body.appendChild(div);
+
+        externalPreviewPopup = document.getElementById('externalPreviewPopup');
+        externalPreviewPopup.querySelector('.close').addEventListener('click', close);
+    };
+
+    var openPreviewPopup = function(html) {
+        if (!externalPreviewPopup) {
+            initPreviewPopup();
+        }
+        updateContent(html);
+        externalPreviewPopup.style.visibility = 'visible';
+    };
+
+    var updateContent = function(html) {
+        var iframeDesktop = document.querySelector('#iframeDesktop');
+        iframeDesktop.contentWindow.document.open('text/html', 'replace');
+        iframeDesktop.contentWindow.document.write(html);
+        iframeDesktop.contentWindow.document.close();
+
+        var iframeMobile = document.querySelector('#iframeMobile');
+        iframeMobile.contentWindow.document.open('text/html', 'replace');
+        iframeMobile.contentWindow.document.write(html);
+        iframeMobile.contentWindow.document.close();
+    };
+
+    return {
+        openPreviewPopup: openPreviewPopup
+    };
+})();
