b2fa7e1b24f19c041fb44f28ec9d8380406519b2
igor <igor.gripas@ardas.dp.ua>
2023-02-27 12:24:36 +0200

    PLUGIN-147: Added background image control example

--- extensions/extension-structure-like-block/src/js/const.js
+++ extensions/extension-structure-like-block/src/js/const.js
@@ -2,4 +2,5 @@ export const BLOCK_UNIQUE_CLASS_NAME = `structure-like-block`;
 
 export const CONTROL_NAME_PREFIX = `structureLikeBlock`;
 export const CONTROL_NAME_BLOCK_BACKGROUND_COLOR = `${CONTROL_NAME_PREFIX}_backgroundColor_control`;
+export const CONTROL_NAME_BLOCK_BACKGROUND_IMAGE = `${CONTROL_NAME_PREFIX}_backgroundImage_control`;
 
